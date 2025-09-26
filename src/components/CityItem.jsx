d4eea3e559ca3f0a0a3f5bdb3c9e90533f36a391
shallowaria <shallowaria@gmail.com>
2025-09-26 21:24:37 +0800

    fix:sidebar uppercase

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import styles from "./CityItem.module.css";
-import { CitiesProvider, useCities } from "../contexts/CitiesContext";
+import { useCities } from "../contexts/CitiesContext";
 
 const formatDate = (date) =>
   new Intl.DateTimeFormat("en", {
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
 import Logo from "./Logo";
-import styles from "./SideBar.module.css";
+import styles from "./Sidebar.module.css";
 
 function Sidebar() {
   return (
